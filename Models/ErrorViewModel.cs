af1d037b211d589127b496eb46e369b90f3fcf74
Yassine Jlassi <yassinej696@gmail.com>
2024-10-16 23:56:37 +0200

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PDF_Text_Extractor_Assistant.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
